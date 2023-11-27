f11f525e51e04d5fcfaede0ffd0974c37d6524b6
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-11-27 13:12:10 -0800

    Added tasks files

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66]
-print(str,"\n", end="")
+print(str)
