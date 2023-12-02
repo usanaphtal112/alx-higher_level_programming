8220de2697d6e54efff475eb3ee0b15dd032e6e8
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-02 11:43:14 -0800

    Addded project files python list, tuple and data structure

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints information about a Python list object.
+ *
+ * @p: A pointer to a Python list object.
+ *
+ * Description:
+ *   This function prints the size of the Python list, the allocated memory,
+ *   and the type name of each element in the list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int elem;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (elem = 0; elem < Py_SIZE(p); elem++)
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add_nodeint - Adds a new node at the beginning of a linked list.
+ *
+ * @head: A pointer to a pointer to the head of the linked list.
+ * @n: The integer value to be stored in the new node.
+ *
+ * Return: A pointer to the newly created node, or NULL if memory allocation fails.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * is_palindrome - Checks if a linked list is a palindrome.
+ *
+ * @head: A pointer to a pointer to the head of the linked list.
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
+
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
+}
