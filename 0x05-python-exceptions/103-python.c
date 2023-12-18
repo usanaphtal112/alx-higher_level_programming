54358c94aa10a5c382ebfc2deedeeb118c035c15
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-18 04:01:12 -0800

    Added files for the project

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,55 @@
+/*
+ * File: 103-python.c
+ * Auth: Naphtal USABYUWERA
+ */
+
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - Print information about a Python list.
+ * @p: A pointer to the PyObject representing the Python list.
+ *
+ * This function prints information about a Python list, including its size,
+ * allocation, and the type of each element. It also calls specific functions
+ * to print additional information for elements of type 'bytes' and 'float'.
+ *
+ * Parameters:
+ *   - p: A pointer to the PyObject representing the Python list.
+ *
+ * Return: None
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	fflush(stdout);
+
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(list->ob_item[i]);
+	}
+}
