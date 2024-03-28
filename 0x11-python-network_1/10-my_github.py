587a671c955371c103b2d4942f97ec8428c9e16f
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-03-28 22:35:55 +0530

    Added project files

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script retrieves the user ID from the GitHub
+API using HTTP basic authentication.
+
+Usage:
+    python script_name.py <username> <password>
+
+Arguments:
+    username (str): The GitHub username.
+    password (str): The GitHub password or personal access token.
+
+Output:
+    Prints the user ID of the authenticated user.
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
---
+++ 0x11-python-network_1/103-search_twitter.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This script retrieves tweets matching a specified query using the Twitter API.
+
+Usage:
+    python script_name.py <consumer_key> <consumer_secret> <query>
+
+Arguments:
+    consumer_key (str): The consumer key for the Twitter API.
+    consumer_secret (str): The consumer secret for the Twitter API.
+    query (str): The search query for tweets.
+
+Output:
+    Prints the matched tweets containing the tweet ID, text, and author's name
+"""
+
+import sys
+import base64
+import requests
+
+
+if __name__ == "__main__":
+    # Get bearer token
+    url = "https://api.twitter.com/oauth2/token"
+    token = "{}:{}".format(sys.argv[1], sys.argv[2]).encode("ascii")
+    token = base64.b64encode(token).decode("utf-8")
+    headers = {
+        "Authorization": "Basic {}".format(token),
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    }
+    payload = {"grant_type": "client_credentials"}
+    r = requests.post(url, headers=headers, data=payload)
+    bearer = r.json().get("access_token")
+
+    # Make search request
+    url = "https://api.twitter.com/1.1/search/tweets.json"
+    headers = {"Authorization": "Bearer {}".format(bearer)}
+    params = {"q": sys.argv[3], "count": "5"}
+    url = "https://api.twitter.com/1.1/search/tweets.json"
+    tweets = requests.get(url, headers=headers, params=params)
+
+    # Print matched tweets
+    tweets = tweets.json().get("statuses")
+    for t in tweets:
+        tweet_id = t.get("id")
+        tweet_text = t.get("text")
+        tweet_author = t.get("user").get("name")
+        print("[{}] {} by {}".format(tweet_id, tweet_text, tweet_author))
