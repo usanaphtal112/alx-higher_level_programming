aafdf5689c68c0cbb0a9479e6797dbd6fd2d5722
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-04-24 10:35:24 -0700

    Added file system text and formated files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 
 const filesys = require("fs");
 filesys.readFile(process.argv[2], "utf-8", function (err, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(data);
 });
