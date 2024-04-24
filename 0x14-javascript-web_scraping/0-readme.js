1031707a1cfd34e8540842369cf96ebb44cf9b0a
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-04-24 10:49:29 -0700

    Added files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,11 @@
 // Read from file
 
 const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
-});
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
