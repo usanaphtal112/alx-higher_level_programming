ed92e1c7e927e1ad3bd0242657476a80e53a89e3
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-04-24 10:39:50 -0700

    Added file system text and formated files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Read from file
 
-const filesys = require'fs');
+const filesys = require('fs');
 filesys.readFile(process.argv[2], 'utf-8', function (err, data) {
 	if (err) {
 		console.log(err);
